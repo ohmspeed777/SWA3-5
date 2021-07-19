1b07036e5738eccd3be47bd3f516d7114a95f7c7
NATTHASITH VORANITIYAOWAPA <61766926+ohmspeed777@users.noreply.github.com>
2021-07-19 15:47:16 +0700

    Initial commit

---
+++ src/com/journaldev/composition/TestPerson.java
@@ -0,0 +1,11 @@
+
+package com.journaldev.composition;
+
+public class TestPerson {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        long salary = person.getSalary();
+    }
+
+}
